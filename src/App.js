286628aebfb598ebf82048930bf2841e91c47349
VijaybaskarA <90896202+VijaybaskarA@users.noreply.github.com>
2021-09-18 02:15:14 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,9 +9,58 @@ import {
 } from "@material-ui/core";
 import * as React from "react";
 import MenuItem from "@mui/material/MenuItem";
+
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const currencies = [
+  {
+    value: 'a',
+    label: 'reddy'
+  },
+  {
+    value: 'b',
+    label: 'prasanth'
+  },
+  {
+    value: 'c',
+    label: 'setti'
+  },
+]
+const steps = [
+  '0%',
+  '',
+  '',
+  '50',
+  '',
+  '',
+  '100%',
+
+];
+
+
 export default function App() {
-  return (
+  const [currency, setCurrency] = React.useState('');
+
+  const handleChange = (event) => {
+    setCurrency(event.target.value);
+  }
+    return (
+       
+
+      <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+      >
     <div className="App">
+    
       <AppBar>
         <toolbar>
           <h1>SIGNIN FORM </h1>
@@ -20,19 +69,59 @@ export default function App() {
 
       <Typography variant="h5">BASIC WITH MATERIAL UI</Typography>
       <form>
-        <TextField
+      <TextField
           style={{ width: "200px", margin: "5px" }}
           type="text"
           label="setgoal"
           variant="outlined"
         />
-        <br />
+      <TextField
+          id="select name"
+          select
+          label=" catagory"
+          value={currency}
+          onChange={handleChange}
+          helperText="Please select your category "
+        >
+          {currencies.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+
         <TextField
-          style={{ width: "200px", margin: "5px" }}
-          type="text"
-          label="goal description"
-          variant="outlined"
-        />
+          id="select name"
+          select
+          label="Select"
+          label="Diversity catagory"
+          value={currency}
+          onChange={handleChange}
+          helperText="Please select your diversity"
+        >
+          {currencies.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+   
+
+        { 
+    <Box sx={{ width: '70%' }}>
+      <Stepper activeStep={0} alternativeLabel>
+        {steps.map((label) => (
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+    </Box>
+   }
+
+
+        
+       
         <br />
         <TextField
           style={{ width: "200px", margin: "5px" }}
@@ -46,6 +135,13 @@ export default function App() {
           type="text"
           label="Attribute"
           variant="outlined"
+          id="select name"
+          select
+          label="Diversity catagory"
+          value={currency}
+          onChange={handleChange}
+          helperText="Please select your "
+          
         />
         <br />
         <TextField
@@ -74,5 +170,6 @@ export default function App() {
         </Button>
       </form>
     </div>
+    </Box>
   );
-}
+  }
